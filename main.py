a529524dac8984afa07f919682698f2a07f3e6bc
Kitwscw <159850948+Kitwscw@users.noreply.github.com>
2024-06-20 11:10:45 +1200

    changed encounter loop

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ NAME2 = ["River", "Pond", "Forest", "Clearing", "Pathway", "Cave", "Grave", "Sta
 #-----FUNCTIONS-----
 
 #-----MAIN CODE-----
+enc=0
 print("WARNING: This game includes descriptions of graphic violence, & animal cruelty & is not suitable for children below the age of 13")
 play=input("do you still wish to play this game?").lower()
 if play =="yes".lower():
@@ -14,14 +15,15 @@ if play =="yes".lower():
     print("you continued walking for a while until you ralised that you had got lost without noticing")
     print("after panicking for a moment, you managed to gather your thoughts and decide that you should first find a place to stay")
     print("& so you set out into the woods, with nothing but your trusty knife in hand & the clothes on your back ")
-else:
-    print("Sorry you dont wanna play the game I guess")
+    while enc<=8:
 
-while True:
-    print(f"you enter {random.choice(NAME1)} {random.choice(NAME2)}")
-    input("slauy")
+        print(f"you enter {random.choice(NAME1)} {random.choice(NAME2)}")
+        input("slauy")
 #   Safe space (1)
 #   if store = true: options to use stuff
 #   keep track of money and health
 
 #   Enemy 
+else:
+    print("Sorry you dont wanna play the game I guess")
+
