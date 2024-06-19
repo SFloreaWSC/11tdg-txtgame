d03b700b0947cbee53f4a12ba253f88f401a9c35
SFloreaWSC <floreas@wsc.school.nz>
2024-06-20 11:47:36 +1200

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ moon = 0
 print("WARNING: This game includes descriptions of graphic violence, & animal cruelty & is not suitable for children below the age of 13")
 play=input("do you still wish to play this game?").lower()
 
-if play == "yes".lower():
+if play == "yes":
     slay = input("AAAAAAAA").lower()
     print(slay)
     print("You were on a path through the woods, a cool breeze carried the scent of pine needles through the air,")
@@ -27,8 +27,11 @@ if play == "yes".lower():
             #store stuff
             print("you come across store")
             opt = input("Enter store or Continue down path").lower()
-            if opt == "enter":
-                print("slauy")
+            if opt == "enter" or opt == "enter store":
+                pri = random.randint(50,100)
+                print(f"Item 1 healing {pri} coins")
+                print(f"item 2 attack {pri} coins")
+                print(f"item 3 defence {pri} coins")
         elif location == "item":
             #item stuff
             print("slay")
