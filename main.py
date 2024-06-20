5cfb769fd1769909f12ed9116bfecf64186dda40
SFloreaWSC <floreas@wsc.school.nz>
2024-06-20 22:29:38 +1200

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ import random
 NAME1 = ["Crystal", "Tranquil", "Gloomy", "Luminous", "Somber", "Crimson", "Jagged", "Dilapidated"]
 NAME2 = ["River", "Pond", "Forest", "Clearing", "Pathway", "Cave", "Grave", "Statue"]
 LOCT = ["store", "item", "enemy"]
+ENE = ["bat", "chamois", "lynx", "wolf"]
 #end of lists
 #-----FUNCTIONS-----
-
+def wares():
+    print(f"An elixer of health, for {random.randint(5,20)} coins.")
+    print(f"I can sharpen your weapon {random.randint(5,20)} coins.")
+    print(f"A patch for your garments {random.randint(5,20)} coins.")
 #-----MAIN CODE-----
 enc = 8
 moon = 0
@@ -15,35 +19,36 @@ print("Also none of the actions depicted in this game should actually be done")
 play=input("do you still wish to play this game?").lower()
 
 if play == "yes":
-    slay = input("AAAAAAAA").lower()
-    print(slay)
     print("You were on a path through the woods, a cool breeze carried the scent of pine needles through the air,")
     print("you continued walking for a while until you ralised that you had got lost without noticing,")
     print("after panicking for a moment, you managed to gather your thoughts and decide that you should first find a place to stay,")
-    print("& so you set out into the woods, with nothing but your trusty knife in hand & the clothes on your back,")
+    print("and so you set out into the woods, with nothing but your trusty knife in hand & the clothes on your back,")
+    
     while enc>=8:
         print(f"You enter the {random.choice(NAME1)} {random.choice(NAME2)}.")
         location = random.choice(LOCT)
         if location == "store":
             #store stuff
             print("you come across store")
-            opt = input("Enter store or Continue down path").lower()
+            opt = input("Enter store or Continue down path\n").lower()
             if opt == "enter" or opt == "enter store":
-                pri = random.randint(50,100)
-                print(f"Item 1 healing {pri} coins")
-                print(f"item 2 attack {pri} coins")
-                print(f"item 3 defence {pri} coins")
+                print("Blah blah blah I have wares and services, for a price of course. But you may only have one, must leave some for others you know")
+                wares()
+                buy = input("Would you like to purchase any of my items?\n").lower()
+                if buy == "elixer":
+                    print("You drink his elixer, as you do you feel QWERTY")
+                    health =+ random.randint(30,50)
+            if opt == "continue" or opt == "continue down" or opt == "continue down path":
+                print("You decide not to enter store you condtinue down the path")
         elif location == "item":
             #item stuff
             print("slay")
         elif location == "enemy":
             #enemy stuff
             print("slay")
-#   Safe space (1)
-#   if store = true: options to use stuff
-#   keep track of money and health
+        #   Enemy 
+        print("As you walk there appeasrs an enemtny")
 
-#   Enemy 
 else:
     print("Sorry you dont wanna play the game I guess")
 
